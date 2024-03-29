203b0724c56dac55b19465b9eb747919493d9006
peter <peter@25sprout.com>
2024-03-29 19:21:01 +0800

    feat: cloak plugin

---
+++ after/plugin/cloak.lua
@@ -0,0 +1,13 @@
+require("cloak").setup({
+	enable = true,
+	cloak_character = "*",
+
+	hightlight_group = "Comment",
+	cloak_length = nil,
+	patterns = {
+		{
+			file_pattern = ".env*",
+			cloak_pattern = "=.+",
+		},
+	},
+})
